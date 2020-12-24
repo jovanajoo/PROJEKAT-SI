71d32c915b993d7f1a39bafba2a090679a9d7290
dpetric70 <70-2018@ftn.edu.rs>
2020-12-24 22:01:30 +0100

    Kreiranje baze podataka i tabela unutar nje.

---
+++ Tabele/dbo.Customers.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Customers]
+(
+	[CustomerID] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [Surname] NVARCHAR(50) NOT NULL, 
+    [E-mail] NVARCHAR(40) NOT NULL, 
+    [City] NVARCHAR(50) NOT NULL, 
+    [Address] NVARCHAR(30) NOT NULL, 
+    [Phone_number] NVARCHAR(15) NOT NULL, 
+    [OrderID] INT NOT NULL, 
+    CONSTRAINT [FK_Customers_ToOrders] FOREIGN KEY ([OrderID]) REFERENCES [Orders]([OrderID]),
+
+
+)
---
+++ Tabele/dbo.Orders.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Orders]
+(
+	[OrderID] INT NOT NULL PRIMARY KEY, 
+    [Order_Date] NCHAR(10) NOT NULL, 
+    [Delivery_Date] NCHAR(10) NOT NULL 
+
+
+);
