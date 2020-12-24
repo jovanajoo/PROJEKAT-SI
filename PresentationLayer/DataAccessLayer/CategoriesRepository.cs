d86dc6f384df1228b81ba7c99ff38062cf4a6a96
dpetric70 <70-2018@ftn.edu.rs>
2020-12-24 22:16:37 +0100

    Kreiranje Repository klase i metoda unutar nje i klase Constants za connectionString

---
+++ PresentationLayer/DataAccessLayer/CategoriesRepository.cs
@@ -0,0 +1,112 @@
+﻿using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class CategoriesRepository
+    {
+        public List<Categories> GetAllCategories()
+        {
+            List<Categories> results = new List<Categories>();
+            using (SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {  
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText = "SELECT * FROM Categories";
+
+                sqlConnection.Open();
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                while (sqlDataReader.Read())
+                {
+                    Categories c = new Categories();
+                    c.CategoryID = sqlDataReader.GetInt32(0);
+                    c.Name = sqlDataReader.GetString(1);
+                    c.Description = sqlDataReader.GetString(2);
+
+                    results.Add(c);
+                }
+            }
+            return results;
+        }
+        public int InsertCategories(Categories c)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText =
+                    string.Format("INSERT INTO Categories VALUES('{0}', '{1}')",
+                    c.Name, c.Description);
+                return sqlCommand.ExecuteNonQuery();
+            }
+        }
+        public int deleteCategoriesById(int CategoriesID)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection())
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText =
+                string.Format("DELETE FROM Categories WHERE CatgeoriesID = {0}", CategoriesID);
+
+                sqlConnection.Open();
+
+                return sqlCommand.ExecuteNonQuery();
+
+            }
+        }
+        public Categories getCategoriesById(int Id)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {
+
+    
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText =
+                string.Format(" SELECT * FROM Categories WHERE Id = {0}", Id);
+
+                sqlConnection.Open();
+
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                sqlDataReader.Read();
+                Categories c = new Categories();
+                c.CategoryID = sqlDataReader.GetInt32(0);
+                c.Name = sqlDataReader.GetString(1);
+                c.Description = sqlDataReader.GetString(2);
+            
+
+                return c;
+            }
+        }
+
+        public int updateCategoriesById(Categories c)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText =
+                string.Format(" UPDATE Categories SET Name = '{0}', Description = '{1}', WHERE CategoryId={2}", c.Name, c.Description);
+
+                sqlConnection.Open();
+
+                return sqlCommand.ExecuteNonQuery();
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ PresentationLayer/DataAccessLayer/ProductsRepository.cs
@@ -0,0 +1,110 @@
+﻿using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class ProductsRepository
+    {
+        public List<Products> GetAllProducts()
+        {
+            List<Products> results = new List<Products>();
+            using (SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText = "SELECT * FROM Products";
+
+                sqlConnection.Open();
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                while (sqlDataReader.Read())
+                {
+                    Products p = new Products();
+                    p.ProductID = sqlDataReader.GetInt32(0);
+                    p.Name = sqlDataReader.GetString(1);
+                    p.Price = sqlDataReader.GetDouble(2);
+                    p.Size = sqlDataReader.GetInt32(3);
+                    p.Description = sqlDataReader.GetString(4);
+
+
+                    results.Add(p);
+                }
+            }
+            return results;
+        }
+        public int deleteProductsById(int ProductID)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection())
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText =
+                string.Format("DELETE FROM Customers WHERE ProductID = {0}", ProductID);
+
+                sqlConnection.Open();
+
+                return sqlCommand.ExecuteNonQuery();
+
+            }
+        }
+        public int InsertProducts(Products p)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText =
+                    string.Format("INSERT INTO Products VALUES('{0}', '{1}','{2}', '{3}')",
+                    p.Name, p.Price, p.Size, p.Description);
+                return sqlCommand.ExecuteNonQuery();
+            }
+        }
+        public Products getProductsById(int ProductID)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText =
+                string.Format(" SELECT * FROM Products WHERE ProductID = {0}", ProductID);
+
+                sqlConnection.Open();
+
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                sqlDataReader.Read();
+                Products p = new Products();
+                p.ProductID = sqlDataReader.GetInt32(0);
+                p.Name = sqlDataReader.GetString(1);
+                p.Price = sqlDataReader.GetDouble(2);
+                p.Size = sqlDataReader.GetInt32(3);
+                p.Description = sqlDataReader.GetString(4);
+
+                return p;
+                
+            }
+
+        }
+        public int updateCProductsById(Products p)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText =
+                string.Format(" UPDATE Products SET Name = '{0}', Price = {1},Size ={2},Description ='{3}'"
+                , p.Name,p.Price,p.Size,p.Description);
+
+                sqlConnection.Open();
+
+                return sqlCommand.ExecuteNonQuery();
+            }
+        }
+    }
+}
+
