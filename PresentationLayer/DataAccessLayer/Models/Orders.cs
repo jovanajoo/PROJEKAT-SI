69b34ae3884f8aa9391d8cdad9ae31c7386ce642
dpetric70 <70-2018@ftn.edu.rs>
2020-12-24 22:06:27 +0100

    Kreiranje model klasa

---
+++ PresentationLayer/DataAccessLayer/Models/Orders.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class Orders
+    {
+        public int OrderID { get; set; }
+        public string Order_Date { get; set; }
+        public string Delivery_Date { get; set; }
+    }
+}
---
+++ PresentationLayer/DataAccessLayer/Models/Products.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class Products
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Size { get; set; }
+        public string Description { get; set; }
+    }
+}
