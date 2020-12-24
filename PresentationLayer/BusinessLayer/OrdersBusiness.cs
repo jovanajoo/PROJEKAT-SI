9e37ad9919bcde53d4bce81a0344bb59ae0d99f3
DESKTOP-T8RLACA\Dragana <draganaobradovic21@gmail.com>
2020-12-25 00:55:57 +0100

    Formirana je klasa OrdersBusiness sa potrebnim metodama.

---
+++ PresentationLayer/BusinessLayer/OrdersBusiness.cs
@@ -0,0 +1,73 @@
+﻿using DataAccessLayer;
+using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    class OrdersBusiness
+    {
+        public OrdersRepository ordersRepository;
+        public OrdersBusiness()
+        {
+            this.ordersRepository = new OrdersRepository();
+        }
+
+        public List<Orders> getAllOrders()
+        {
+            return this.ordersRepository.GetAllOrders();
+        }
+
+
+
+        public bool InsertOrders(Orders o)
+        {
+            if (this.ordersRepository.InsertOrders(o) > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+        public bool deleteOrdersById(int OrderID)
+        {
+            if (this.ordersRepository.deleteOrdersById(OrderID) > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+        public Orders getOrdersById(int OrderID)
+        {
+            return this.ordersRepository.GetAllOrders().Where(o => o.OrderID == OrderID).First();
+        }
+
+
+
+        public bool updateOrdersById(Orders o)
+        {
+            if (this.ordersRepository.updateOrdersById(o) > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
