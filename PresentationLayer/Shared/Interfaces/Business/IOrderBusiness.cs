2422b2a513976372065ff1ea98b6f94f49a0af15
DESKTOP-T8RLACA\Dragana <draganaobradovic21@gmail.com>
2021-01-11 21:12:24 +0100

    Kreiranje interfejsa.

---
+++ PresentationLayer/Shared/Interfaces/Business/IOrderBusiness.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Interfaces.Business
+{
+    public interface IOrderBusiness
+    {
+        List<Order> GetAllOrders();
+        int InsertOrders(Order o);
+        int DeleteOrdersById(int OrderID);
+        Order GetOrdersById(int OrderID);
+        int UpdateOrdersById(Order o);
+    }
+}
