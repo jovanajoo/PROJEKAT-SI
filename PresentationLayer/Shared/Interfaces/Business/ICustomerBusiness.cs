fc790a0d3192e247ab5a8ece41f88e23c116ab36
DESKTOP-T8RLACA\Dragana <draganaobradovic21@gmail.com>
2021-01-11 21:56:12 +0100

    Ponovno formiranje interfejsa.

---
+++ PresentationLayer/Shared/Interfaces/Business/ICustomerBusiness.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Interfaces.Business
+{
+   public interface ICustomerBusiness
+    {
+        List<Customer> GetAllCustomers();
+        int InsertCustomers(Customer c);
+        int DeleteCustomersById(int Id);
+        Customer GetCustomersById(int Id);
+        int UpdateCustomersById(Customer c);
+    }
+}
