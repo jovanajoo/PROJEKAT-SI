36f26c42fcecd6e16498105165cc40a5b2578c81
dpetric70 <70-2018@ftn.edu.rs>
2021-01-13 13:15:20 +0100

    Neke sitne promene u tabelama Categories i Customers

--- PresentationLayer/Shared/Models/Customer.cs
+++ PresentationLayer/Shared/Models/Customer.cs
@@ -9,7 +9,7 @@ namespace Shared.Models
     public class Customer
     {  
         public int CustomerID { get; set; }
-        public string Username { get; set; }
+        public string UserName { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string City { get; set; }
